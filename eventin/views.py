74bab8268fa5af5357172464147d00c1887f3b69
Francilene Silva <psilva.francilene@gmail.com>
2025-02-06 16:32:38 +0000

    feat: inserindo ordenação, filtro e paginação

--- eventin/views.py
+++ eventin/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, filters
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.views import APIView
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Evento, Participante, Inscricao
 from .serializers import *
@@ -11,23 +12,28 @@ class EventoViewSet(viewsets.ModelViewSet):
     # autenticação
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
-
     queryset = Evento.objects.all()
     serializer_class = EventoSerializer
+    # filtro e ordenação
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    ordering_fields = ["titulo"]
+    search_fields = ["titulo", "local"]
 
 class ParticipanteViewSet(viewsets.ModelViewSet):
     # autenticação
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
-
     queryset = Participante.objects.all()
     serializer_class = ParticipanteSerializer
+    # filtro e ordenação
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    ordering_fields = ["nome"]
+    search_fields = ["nome", "cpf"]
 
 class InscricaoViewSet(viewsets.ModelViewSet):
     # autenticação
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAdminUser] # só acessa se o usuário for admin
-
     queryset = Inscricao.objects.all()
     serializer_class = InscricaoSerializer
 
--- setup/settings.py
+++ setup/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'eventin',
     'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # inserindo paginação
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
+}
